a6772f6a283f3ef7ab426e04e96a2c4685c23039
MarlonMoreno <marlonmoreno894@gmail.com>
2021-06-04 14:45:25 -0500

    get info from api and parse it with function

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,23 @@
 class Movie < ApplicationRecord
+    has_many :genres
+
+
+    def self.get_movie_info 
+
+
+        img_url = "https://image.tmdb.org/t/p/w300"
+        url = URI("https://api.themoviedb.org/3/movie/top_rated?api_key=6cb7579cc35c7ba28cadfc4c84a1227d&language=en-US&page=1")
+
+        response = Net::HTTP.get(url)
+        info = JSON.parse(response)
+
+    end
+
+    def self.make_movie
+
+    end
+
+    def make_genre
+
+    end
 end
