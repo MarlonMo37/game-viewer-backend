4e164a7491d2cbd310a372183ce73b879649b4dc
MarlonMoreno <marlonmoreno894@gmail.com>
2021-06-04 15:09:46 -0500

    make tables, and make joint table

--- db/migrate/20210604190806_create_movies.rb
+++ db/migrate/20210604190806_create_movies.rb
@@ -1,6 +1,13 @@
 class CreateMovies < ActiveRecord::Migration[6.1]
   def change
     create_table :movies do |t|
+      t.string :title
+      t.string :poster_url
+      t.boolean :favorite?
+      t.text :summary
+      t.string :rating
+      t.string :release_date
+
 
       t.timestamps
     end
---
+++ db/migrate/20210604200823_create_movie_genres.rb
@@ -0,0 +1,10 @@
+class CreateMovieGenres < ActiveRecord::Migration[6.1]
+  def change
+    create_table :movie_genres do |t|
+      t.references :movie, index: true, foreign_key: true
+      t.references :genre, index: true, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
