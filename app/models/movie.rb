c03d155df1a76aca9030f468e497da6e35c9a35d
MarlonMoreno <marlonmoreno894@gmail.com>
2021-06-09 11:41:33 -0500

    make class method that takes care of organizing and creating movies

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,11 +13,22 @@ class Movie < ApplicationRecord
 
         response = Net::HTTP.get(url)
         info = JSON.parse(response)
-
+        self.make_movie(info["results"])
     end
 
-    def self.make_movie
+    def self.make_movie(movies)
+        img_url = "https://image.tmdb.org/t/p/w300"
+        
+        movies.each do |movie|
+           movie_info = {}
+           movie_info["title"] = movie["title"]
+           movie_info["poster_url"] = img_url + movie["poster_path"]
+           movie_info["summary"] = movie["overview"]
+           movie_info["audience_rating"] = movie["vote_rating"]
+           movie_info["release_date"] = movie["release_date"]
 
+           self.find_or_create_by(movie_info)    
+        end
     end
 
     def make_genre
--- db/migrate/20210604190806_create_movies.rb
+++ db/migrate/20210604190806_create_movies.rb
@@ -3,9 +3,8 @@ class CreateMovies < ActiveRecord::Migration[6.1]
     create_table :movies do |t|
       t.string :title
       t.string :poster_url
-      t.boolean :favorite?
       t.text :summary
-      t.string :rating
+      t.float :audience_rating
       t.string :release_date
 
 
