a622e863b7a070f0d2d776a295c60916eb69c623
MarlonMoreno <marlonmoreno894@gmail.com>
2021-07-05 21:50:32 -0500

    Make movei serializer show id

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,6 +1,6 @@
 class MovieSerializer < ActiveModel::Serializer
   # include JSONAPI::Serializer
-  attributes :title, :poster_url, :summary, :audience_rating, :release_date
+  attributes :title, :poster_url, :summary, :audience_rating, :release_date, :id
   has_many :genres
   has_many :reviews
   
