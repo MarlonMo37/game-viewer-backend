5dffdef6b89ba70ab16b9984545a0999efd31e94
MarlonMoreno <marlonmoreno894@gmail.com>
2021-07-08 14:25:27 -0500

    Make movie_id accesable in serializer

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,6 +1,6 @@
 class ReviewSerializer < ActiveModel::Serializer
   # include JSONAPI::Serializer
-  attributes :written_review, :rating, :id
+  attributes :written_review, :rating, :id, :movie_id
 
   belongs_to :movie, except: [:created_at, :updated_at]
 
